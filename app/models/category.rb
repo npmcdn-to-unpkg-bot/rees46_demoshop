9f5141e6a8dc27e2033beaa4c692f16670ba1805
Saroar <saroar9@gmail.com>
2016-01-26 15:48:30 +0300

    update product and category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ActiveRecord::Base
   belongs_to :parent, class_name: 'Category', foreign_key: :parent_id
-
+  has_many :products
   validates :name, presence: true
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+  belongs_to :category
+
+  validates :title, presence: true
+  validates :image, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :brand, presence: true
+
 end
