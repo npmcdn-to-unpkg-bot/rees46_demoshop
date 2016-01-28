3a6fa9a639460fc3d0ccb5b5f4a578e263edbd73
Saroar <saroar9@gmail.com>
2016-01-28 15:19:00 +0300

    add private mmethod in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,23 @@ class Product < ActiveRecord::Base
   belongs_to :category
   has_many :line_items
 
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, presence: true
   validates :image, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :brand, presence: true
   mount_uploader :image, ImageUploader
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, "Line Items present")
+      return false
+    end
+  end
 end
