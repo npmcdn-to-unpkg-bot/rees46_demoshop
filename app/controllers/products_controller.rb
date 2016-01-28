0e895284af0c2c86ae0307f6805c6c9fd27c887b
Saroar <saroar9@gmail.com>
2016-01-29 00:05:07 +0300

    fixed spell mistake in product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, :expect => [:index, :show]
+  before_action :authenticate_user!, :except => [:index, :show]
+
   def index
     @products = Product.all
   end
