470312643c03c08bc2bcc450df4d43cd893c2788
Saroar <saroar9@gmail.com>
2016-01-28 15:50:27 +0300

    add add to cart button for products in store index viewa and create mdethod in line item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,2 +1,16 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: :create
+  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+
+  def create
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
+
+    if @line_item.save
+      redirect_to @line_item.cart, notice: "Line Item was successfully created."
+    else
+      render action: 'new'
+    end
+  end
 end
