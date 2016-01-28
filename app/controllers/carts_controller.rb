5b9a3854a8d3ad541c95c2bf692ac0231c54af70
Saroar <saroar9@gmail.com>
2016-01-28 15:56:10 +0300

    update cart controller and show page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,12 @@
 class CartsController < ApplicationController
+  before_action :set_cart, only: [:show]
+
   def show
+  end
+
+  private
+  
+  def set_cart
+    @cart = Cart.find(params[:id])
   end
 end
