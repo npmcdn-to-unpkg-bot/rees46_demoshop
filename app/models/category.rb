f2c236c58f7ee3d24e6bc6e45c8d8ff22266d83f
Saroar <saroar9@gmail.com>
2016-02-01 11:46:26 +0300

    category model style update

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,8 @@ class Category < ActiveRecord::Base
   acts_as_tree order: 'name'
   has_many :products
   validates :name, presence: true
-  has_many :children, class_name: 'Category', foreign_key: :parent_id, dependent: :destroy
+
   belongs_to :parent, class_name: 'Category'
+  has_many :children, class_name: 'Category', foreign_key: :parent_id, dependent: :destroy
+
 end
