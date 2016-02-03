e20f9bed8d9e583db4b181eb982ba889bb1489b2
Saroar <saroar9@gmail.com>
2016-02-03 17:00:19 +0300

    add gender params in controller and inside form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -58,6 +58,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:title, :image, :description, :price, :brand, :category_id)
+    params.require(:product).permit(:title, :image, :description, :price, :brand, :category_id, :gender)
   end
 end
