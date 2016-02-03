b14d9b125f160d9090715b78f3c39c8e0c651efc
Saroar <saroar9@gmail.com>
2016-02-03 11:17:38 +0300

    add comment in product model for price :)

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ActiveRecord::Base
 
   validates :title, :image, :description, :brand, :category_id, presence: true
   validates :title, uniqueness: true
+  # Price should be not less then $100 :) lets do some business 
   validates :price, :presence => true, numericality: { greater_than_or_equal_to: 100 }
   mount_uploader :image, ImageUploader
 
