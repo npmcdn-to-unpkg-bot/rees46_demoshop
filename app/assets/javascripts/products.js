bab49f9a8bf3fbdd81316209c989d9ac1220a48a
Saroar <saroar9@gmail.com>
2016-02-04 18:27:24 +0300

    update form and product js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,9 +1,8 @@
-$
-  $( document ).ready(function() {
-    $('#product_industry').on('change', function() {
-      var industry = $(this).val();
-      if (industry === "fashion") {
-        $('#size-param').removeClass('hidden');
-      }
-    });
+$( document ).ready(function() {
+  $('#product_industry').on('change', function() {
+    var industry = $(this).val();
+    if (industry === "fashion") {
+      $('#size-param').removeClass('hidden');
+    }
   });
+});
