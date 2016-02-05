948a090bbbc59b8f55e5b1d835a0c5f3add4cbda
Saroar <saroar9@gmail.com>
2016-02-05 20:46:01 +0300

    edit code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -58,6 +58,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:title, :image, :description, :price, :brand, :category_id, :industry, :gender, :size, russian_size: [], euro_size: [], british_size: [], american_size: [], asian_size: [] )
+    params.require(:product).permit(:title, :image, :description, :price, :brand, :category_id, :industry, :gender, :size, euro_size: [], british_size: [], american_size: [], asian_size: [] )
   end
 end
