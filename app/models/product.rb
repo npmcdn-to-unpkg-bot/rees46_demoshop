2ddfe5c4f785f6aeea17d4c2403807d38293a5b9
Saroar <saroar9@gmail.com>
2016-02-05 16:51:42 +0300

    update code lines

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,28 +1,21 @@
 class Product < ActiveRecord::Base
   indus_arr = %w[fashion cosmetic kids].map(&:capitalize)
-  enum industry: indus_arr
-
   gender_arr = %w[unisex male female].map(&:capitalize)
-  enum gender: gender_arr
-
-  type_arr = [:shoe, :shirt, :tshirt, :underwear, :trouser, :jacket, :blazer, :sock, :belt, :hat, :glove].map(&:capitalize)
-  enum type: type_arr
-
-  enum size: [:ru, :eu, :us, :uk, :az]
-
+  type_arr = %w[shoe shirt tshirt underwear trouser jacket blazer sock belt hat glove].map(&:capitalize)
   ru_arr = %w[35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56]
-  enum russian_size: ru_arr
-
   eu_arr = %w[e36 e37 e38 e39 e40 e41 e42 e43 e44]
-  enum euro_size: eu_arr
-
   us_arr = %w[XS S M L XL XXL]
-  enum american_size: us
-
   uk_arr = ["b3", "b4", "b5", "d5.5", "b6", "b6.5", "b7", "b7.5", "b8" ]
-  enum british_size: uk_arr
-
   az_arr = %w[a35 a36 a37 a38 a39 a40 a41 a42 a43 a44 a45 a46 a47 a48 a49 a50 a51 a52 a53 a54 a55 a56]
+
+  enum industry: indus_arr
+  enum gender: gender_arr
+  enum type: type_arr
+  enum size: [:ru, :eu, :us, :uk, :az]
+  enum russian_size: ru_arr
+  enum euro_size: eu_arr
+  enum american_size: us
+  enum british_size: uk_arr
   enum asia_size: az_arr
 
 
@@ -39,23 +32,14 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ImageUploader
 
   INDUSTRY_TYPES = indus_arr
-
   FASHION_TYPEES = [["Gender", "gender"], ["Type", "type"], ["Size", "size"]]
-
   GENDER_TYPES = gender_arr
-
   TYPE_TYPES = type_arr
-
   SIZE_TYPES = [["Russian Size", "ru"], ["Euro Size", "eu"], ["American Size", "us"], ["British Size", "uk"], ["Asian size", "az"]]
-
   RU_SIZES = eu_arr
-
   EU_SIZES = eu_arr
-
   UK_SIZES = uk_arr
-
   US_SIZES = us_arr
-
   AISA_SIZES = az_arr
 
   private
