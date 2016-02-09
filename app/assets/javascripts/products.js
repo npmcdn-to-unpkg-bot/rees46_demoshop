c06f8f75249c7f7b8e2b32970d6f5446ad75dfb7
Saroar <saroar9@gmail.com>
2016-02-09 16:31:37 +0300

    add js code for cosmetic and kids

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -2,7 +2,14 @@ $( document ).on("last-element-rendered", function() {
   $('#product_industry').on('change', function() {
     var industry = $(this).val();
     if (industry === "fashion") {
+      $(".country-sizes").addClass('hidden');
       $('#fashion-param').removeClass('hidden');
+    } else if (industry === "cosmetic") {
+      $(".country-sizes").addClass('hidden');
+      $('#cosmetic-param').removeClass('hidden');
+    } else if (industry === "kids") {
+      $(".country-sizes").addClass('hidden');
+      $('#kids-param').removeClass('hidden');
     }
   });
 
