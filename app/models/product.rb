a1e2b19c543972f51032fda00b2650b783b15244
Saroar <saroar9@gmail.com>
2016-02-10 17:04:04 +0300

    crate volums model for product and add relationsip with product and migrate

--- app/models/product.rb
+++ app/models/product.rb
@@ -160,6 +160,7 @@ class Product < ActiveRecord::Base
 
   belongs_to :category
   has_many :line_items
+  has_many :volumes
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210114723) do
+ActiveRecord::Schema.define(version: 20160210140205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20160210114723) do
     t.integer  "part_types",     default: [], null: false, array: true
     t.integer  "skin_types",     default: [], null: false, array: true
     t.integer  "conditions",     default: [], null: false, array: true
+    t.integer  "volume_id"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
@@ -132,7 +133,18 @@ ActiveRecord::Schema.define(version: 20160210114723) do
 
   add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
+  create_table "volumes", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "value"
+    t.integer  "value_price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "volumes", ["product_id"], name: "index_volumes_on_product_id", using: :btree
+
   add_foreign_key "line_items", "carts"
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
+  add_foreign_key "volumes", "products"
 end
