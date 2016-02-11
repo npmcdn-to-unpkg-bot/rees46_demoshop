d79e21ca064febbac3d92c85a18e448b62358239
Saroar <saroar9@gmail.com>
2016-02-11 17:14:12 +0300

    add class for size types for kids and fashion and update product js

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,13 +3,13 @@ $( document ).on("last-element-rendered", function() {
     var industry = $(this).val();
     if (industry === "fashion") {
       $(".country-sizes, .group-sizes").addClass('hidden');
-      $('#fashion-param, #gender').removeClass('hidden');
+      $('#fashion-param, #gender, #size-types').removeClass('hidden');
     } else if (industry === "cosmetic") {
       $(".country-sizes, .group-sizes").addClass('hidden');
       $('#cosmetic-param, #gender').removeClass('hidden');
     } else if (industry === "kids") {
       $(".country-sizes, .group-sizes").addClass('hidden');
-      $('#kids-param , #gender').removeClass('hidden');
+      $('#kids-param , #gender, #size-types').removeClass('hidden');
     }
   });
 
