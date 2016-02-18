a08ffdc0bf711ea7385badff07054a7fa2a62216
Saroar <saroar9@gmail.com>
2016-02-18 13:49:55 +0300

    create methhod for product validates

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,9 +3,25 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
 
-  validates :size_id, presence: true 
+  validates :size_id, presence: true, if: :nil_sizes?
 
   def total_price
     product.price * quantity
+  end
+
+  private
+
+  def nil_sizes?
+    if self.product.size?
+      true
+    elsif self.product.size
+      true
+    elsif self.product.age_sizes?
+      true
+    elsif self.product.hight_sizes?
+      false
+    else
+      false
+    end
   end
 end
