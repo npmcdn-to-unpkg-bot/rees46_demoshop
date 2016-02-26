a62acb0095ed60a956036d5a586fa39e6e914c14
Saroar <saroar9@gmail.com>
2016-02-26 12:59:33 +0300

    improve line item controler and views

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,12 +1,17 @@
 class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: :create
+  # before_action :find_line_item, only: :increment, :decrement
 
   def create
     product = Product.find(line_item_params[:product_id])
-    @line_item = @cart.line_items.create(line_item_params)
 
-    if @line_item.save
+    @line_item = @cart.line_items.find_or_initialize_by(line_item_params)
+
+    if @line_item.persisted?
+      @line_item.update(quantity: (@line_item.quantity += 1))
+      redirect_to @line_item.cart
+    elsif @line_item.save
       redirect_to @line_item.cart
     else
       flash[:danger] = 'Please select product size !'
@@ -14,7 +19,19 @@ class LineItemsController < ApplicationController
     end
   end
 
+  # def increment
+  #   @line_item.update(quantity: (@line_item.quantity += 1))
+  # end
+  #
+  # def decrement
+  #   @line_item.update(quantity: (@line_item.quantity -= 1))
+  # end
+
   private
+
+  # def find_line_item
+  #   @line_item = cart.line_items.find(params[:line_item_id])
+  # end
 
   def line_item_params
     params.require(:line_item).permit(:product_id, :size_id, :age_sizes )
