ba166b2b197e9a1273bc8a7c5213cbd2338b9776
Saroar <saroar9@gmail.com>
2016-03-18 19:17:01 +0300

    clean code for REES46

--- app/services/products_info.rb
+++ app/services/products_info.rb
@@ -19,37 +19,37 @@ class ProductsInfo
     products
   end
 
-  def get_cart_products_info
-    @line_items.each do |l|
-      product = {}
-      product[:item_id] = l.product.id
-      product[:price] = l.product.price
-      product[:is_available] = 1
-      product[:categories] = [l.product.category_id]
-      products << product
-    end
-    products
-  end
-
-  def get_similer_products_info
-    @line_items.each do |l|
-      product = {}
-      product[:cart] = l.product.id
-      product[:categories] = l.product.category_id
-      products << product
-    end
-    products
-  end
-
-  def get_cart_products_ids
-    @line_items.map do |li|
-      li.product_id
-    end
-  end
-
-  def get_categories_ids
-    @line_items.map do |li|
-      li.product.category_id
-    end.join(',')
-  end
+  # def get_cart_products_info
+  #   @line_items.each do |l|
+  #     product = {}
+  #     product[:item_id] = l.product.id
+  #     product[:price] = l.product.price
+  #     product[:is_available] = 1
+  #     product[:categories] = [l.product.category_id]
+  #     products << product
+  #   end
+  #   products
+  # end
+  #
+  # def get_similer_products_info
+  #   @line_items.each do |l|
+  #     product = {}
+  #     product[:cart] = l.product.id
+  #     product[:categories] = l.product.category_id
+  #     products << product
+  #   end
+  #   products
+  # end
+  #
+  # def get_cart_products_ids
+  #   @line_items.map do |li|
+  #     li.product_id
+  #   end
+  # end
+  #
+  # def get_categories_ids
+  #   @line_items.map do |li|
+  #     li.product.category_id
+  #   end.join(',')
+  # end
 end
