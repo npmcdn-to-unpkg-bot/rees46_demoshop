08f4a271602209e9d7c7d650ab6619065428ffba
Saroar <saroar9@gmail.com>
2016-03-18 23:14:02 +0300

    create medhod in cart model and fixed some rees46 product id issue

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,4 +8,16 @@ class Cart < ActiveRecord::Base
   def total_quantity
     line_items.to_a.sum(&:total_quantity)
   end
+
+  def product_ids_in_cart
+    line_items.collect {|li| li.product_id }
+  end
+
+  def product_price_in_cart
+    line_items.map {|li| li.product.price.to_i }
+  end
+
+  def category_ids_in_cart
+    line_items.map {|li| li.product.category_id }
+  end
 end
