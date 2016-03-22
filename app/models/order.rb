a7d80cb3dc11a4c91e3dc8754a05bbcbd5570b8e
Saroar <saroar9@gmail.com>
2016-03-22 15:47:59 +0300

    decrement product sotck after order complite and show stock numbers in products show page

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,23 @@
 class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
+
   validates :name, :address, :email, presence: true
+
+  after_save :decrement_product_stock
 
   def add_line_items_form_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
       line_items << item
+    end
+  end
+
+  private
+
+  def decrement_product_stock
+    self.line_items.each do |li|
+      binding.pry
+      li.product.update(stock: li.product.stock - li.quantity)
     end
   end
 end
