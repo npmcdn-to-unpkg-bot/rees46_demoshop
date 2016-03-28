c213e0be37a36daa8e5548f809f94fd1dc2843cc
Saroar <saroar9@gmail.com>
2016-03-28 11:07:37 +0300

    use consistent structure for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,14 @@
 class Category < ActiveRecord::Base
   extend ActsAsTree::TreeWalker
-  acts_as_tree order: 'name'
   extend FriendlyId
+
+  acts_as_tree order: 'name'
+
   friendly_id :name, use: :slugged
 
   has_many :products
-  validates :name, presence: true
-
-  belongs_to :parent, class_name: 'Category'
   has_many :children, class_name: 'Category', foreign_key: :parent_id, dependent: :destroy
+  belongs_to :parent, class_name: 'Category'
+
+  validates :name, presence: true
 end
