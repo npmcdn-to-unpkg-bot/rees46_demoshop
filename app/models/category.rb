7091c2eebac20d1b43fef29874d7521d0c1e3644
Saroar <saroar9@gmail.com>
2016-03-31 16:29:05 +0300

    create new method for category recommender and add in recommender js

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,4 +11,14 @@ class Category < ActiveRecord::Base
   belongs_to :parent, class_name: 'Category'
 
   validates :name, presence: true
+
+  def human_categories_industry
+    if  self.categories_industry == 0
+      return 'fashion'
+    elsif  self.categories_industry == 1
+      return 'cosmetic'
+    else  self.categories_industry == 2
+      return 'child'
+    end
+  end
 end
