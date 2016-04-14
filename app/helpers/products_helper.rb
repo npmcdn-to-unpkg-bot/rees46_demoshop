e12eae68e8d884b00396446ab6bb5e8996c2149c
Saroar <saroar9@gmail.com>
2016-04-14 15:05:59 +0300

    imporve product stock helper method and make product picture looks good

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,7 @@
 module ProductsHelper
-  def print_stock(stock)
-    stock > 0 ? 'In Stock' : 'Out of Stock'
+  def print_stock(product)
+    if product.stock > 0
+      link_to("BUY NOW", product_path(product), class: "btn btn-success")
+    end
   end
 end
