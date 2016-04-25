4c52ec2d79b5170b663e9356cfa5c5c56848208b
Saroar <saroar9@gmail.com>
2016-04-25 22:10:46 +0300

    make looks good popup after click add to cart

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -51,8 +51,25 @@ $( document ).on("last-element-rendered", function() {
 
 
 // Popup cart product
-$(document).ready(function(){
-  $('#btn').click(function(){
-    $('#server_msg_modal').modal('show');
-   });
+
+$(document).ready(function($){
+  //open popup
+  $('.cd-popup-trigger').on('click', function(event){
+    event.preventDefault();
+    $('.cd-popup').addClass('is-visible');
+  });
+
+  //close popup
+  $('.cd-popup').on('click', function(event){
+    if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+      event.preventDefault();
+      $(this).removeClass('is-visible');
+    }
+  });
+  //close popup when clicking the esc keyboard button
+  $(document).keyup(function(event){
+    if(event.which=='27'){
+      $('.cd-popup').removeClass('is-visible');
+    }
+  });
 });
