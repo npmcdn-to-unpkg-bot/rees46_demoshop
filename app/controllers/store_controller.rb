2cdb978dba4fdb9fff7fef8046813d98581a37e3
Saroar <saroar9@gmail.com>
2016-05-06 00:33:41 +0300

    fixed kaminari paginate for REES46 recommended products

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -9,7 +9,7 @@ class StoreController < ApplicationController
       @products << Product.find(id) if Product.where(id: id).any?
     end
 
-    #@products = Product.page(params[:page]).per(24)
+    @products = Kaminari.paginate_array(@products).page(params[:page]).per(4)
 
     @recommender = params[:recommender]
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :orders
 
   get 'store/index'
+  get 'store/font_products_urls'
   post 'store/font_products_urls' => 'store#font_products_urls'
 
   mount RedactorRails::Engine => '/redactor_rails'
