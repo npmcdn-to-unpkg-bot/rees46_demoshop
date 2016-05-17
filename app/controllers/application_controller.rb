2c58d42584844ab86b5596e0d0c33ae066c47392
Saroar <saroar9@gmail.com>
2016-05-17 17:54:46 +0300

    refactor for header menu category code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   include CurrentCart
   before_action :find_cart
+  before_action :find_menu_categories
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -12,5 +13,9 @@ class ApplicationController < ActionController::Base
       else
         root_path
       end
+  end
+
+  def find_menu_categories
+    @menu_categories = Category.where(parent_id: nil).where.not(slug: "categories-industry").order(:id)
   end
 end
