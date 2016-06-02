47e1f31b509619e289756bba8cf34966a0ceb3f3
Saroar <saroar9@gmail.com>
2016-06-02 13:50:54 +0300

    refactor REES46.recommend code

--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -1,9 +1,9 @@
 module RecommendationsHelper
   RECOMMENDER_TYPES = ['interesting', 'also_bought', 'similar', 'popular', 'see_also', 'recently_viewed', 'buying_now']
 
-  def recommended_by
+  def recommended_type_by
     RECOMMENDER_TYPES.each do |recommender_type|
-      recommender_type.to_s
+      recommender_type
     end
   end
 end
