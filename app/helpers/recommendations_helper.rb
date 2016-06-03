d835b2beb9b7ebcf32f27a3016448239a825ecab
Saroar <saroar9@gmail.com>
2016-06-03 11:52:53 +0300

    remove h2 title and hr from home page

--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -3,17 +3,17 @@ module RecommendationsHelper
     @recommended_by = recommended_case
     case @recommended_by
       when 'also_bought'
-        content_tag(:h2, 'С этим товаром покупают', class: ["text-center"])
+        content_tag(:h2, 'С этим товаром покупают')
       when 'interesting'
-        content_tag(:h2, 'Возможно, вам это понравится', class: ["text-center"])
+        content_tag(:h2, 'Возможно, вам это понравится')
       when 'similar'
-        content_tag(:h2, 'Похожие товары', class: ["text-center"])
+        content_tag(:h2, 'Похожие товары')
       when 'popular'
-        content_tag(:h2, 'Популярные товары', class: ["text-center","recommender-block-title"])
+        content_tag(:h2, 'Популярные товары', class: ["recommender-block-title"])
       when 'recently_viewed'
-        content_tag(:h2, 'Вы недавно смотрели', class: ["text-center","recommender-block-title"])
+        content_tag(:h2, 'Вы недавно смотрели', class: ["recommender-block-title"])
       when 'see_also'
-        content_tag(:h2, 'Посмотрите также', class: ["text-center","recommender-block-title"])
+        content_tag(:h2, 'Посмотрите также', class: ["recommender-block-title"])
     end
   end
 end
