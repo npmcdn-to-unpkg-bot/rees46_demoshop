1709d9658017e21ba931516e17f70414fa00c138
Saroar <saroar9@gmail.com>
2016-06-03 11:35:09 +0300

    move get_recommended_case method from ApplicationHelper to RecommendationsHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,22 +25,4 @@ module ApplicationHelper
     end
     flash_messages.join("\n").html_safe
   end
-
-  def get_recommended_case(recommended_case)
-    @recommended_by = recommended_case
-    case @recommended_by
-      when 'also_bought'
-        content_tag(:h2, 'С этим товаром покупают', class: ["text-center"])
-      when 'interesting'
-        content_tag(:h2, 'Возможно, вам это понравится', class: ["text-center"])
-      when 'similar'
-        content_tag(:h2, 'Похожие товары', class: ["text-center"])
-      when 'popular'
-        content_tag(:h2, 'Популярные товары', class: ["text-center","recommender-block-title"])
-      when 'recently_viewed'
-        content_tag(:h2, 'Вы недавно смотрели', class: ["text-center","recommender-block-title"])
-      when 'see_also'
-        content_tag(:h2, 'Посмотрите также', class: ["text-center","recommender-block-title"])
-    end
-  end
 end
