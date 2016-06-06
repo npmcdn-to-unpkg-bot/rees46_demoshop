8255cdf5a1d016a59b639a2cec6683e475626936
Saroar <saroar9@gmail.com>
2016-06-06 19:18:21 +0300

    clean RecommendationsHelper method

--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -1,19 +1,2 @@
 module RecommendationsHelper
-  def get_recommended_case(recommended_case)
-    @recommended_by = recommended_case
-    case @recommended_by
-      when 'also_bought'
-        content_tag(:h2, 'С этим товаром покупают', class: ["recommender-block-title"])
-      when 'interesting'
-        content_tag(:h2, 'Возможно, вам это понравится', class: ["recommender-block-title"])
-      when 'similar'
-        content_tag(:h2, 'Похожие товары', class: ["recommender-block-title"])
-      when 'popular'
-        content_tag(:h2, 'Популярные товары', class: ["recommender-block-title"])
-      when 'recently_viewed'
-        content_tag(:h2, 'Вы недавно смотрели', class: ["recommender-block-title"])
-      when 'see_also'
-        content_tag(:h2, 'Посмотрите также', class: ["recommender-block-title"])
-    end
-  end
 end
